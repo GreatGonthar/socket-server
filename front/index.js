8aff5d03469708c086e013a2e562e504a26ee0fc
GreatGonthar <gontharforeverrules@gmail.com>
2023-06-16 18:55:56 +1000

    sensors test

--- front/index.js
+++ front/index.js
@@ -1,23 +1,163 @@
 const socket = io();
-const messages = document.querySelector(".messages");
-const form = document.querySelector(".form");
-const input = document.querySelector(".input");
-const nameBlock = document.querySelector(".name");
+// const messages = document.querySelector(".messages");
+// const form = document.querySelector(".form");
+// const input = document.querySelector(".input");
+// const nameBlock = document.querySelector(".name");
+const divPlayer2 = document.querySelector(".player2");
+const divPlayer1 = document.querySelector(".player1");
+let count1 = 0;
+let count2 = 0;
+
+let MainplayerName = "";
+let userName1 = "";
+let userName2 = "";
+let count_ball = { x: 0, y: 0 };
+const STEP = 10;
+const WIDTH = 600;
+const HEIGHT = 800;
+const BACKGROUND_COLOR = "silver";
+const PLAYER_SIZE = { x: 100, y: 20 };
+const P1_COLOR = "red";
+const P2_COLOR = "blue";
+const ball = {
+	size: {
+		x: 20,
+		y: 20,
+	},
+	color: "gray",
+};
+
+const myCanvas = document.getElementById("my_canvas");
+myCanvas.autofocus;
+const ctx = myCanvas.getContext("2d");
+myCanvas.width = WIDTH;
+myCanvas.height = HEIGHT;
+myCanvas.style.background = BACKGROUND_COLOR;
 
 const userName = prompt("enter your name:");
-nameBlock.innerHTML = userName;
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	if (input.value) {
-		socket.emit("chat message", { message: input.value, name: userName });
-        input.value = ''
+socket.emit("pong", { value: count1, name: userName });
+
+socket.on("pong", (data) => {
+	if (MainplayerName === "") {
+		MainplayerName = data.mainName;
+		userName1 = MainplayerName;
 	}
+
+	if (data.name === MainplayerName) {
+		count1 = +data.value;
+	} else {
+		count2 = +data.value;
+		userName2 = data.name;
+	}
+
+	// divPlayer2.innerHTML = `${data.
+});
+socket.on("ball", (data) => {
+	count_ball.x = data.ball.x;
+	count_ball.y = data.ball.y;
 });
 
-socket.on('chat message', (data) => {
-    const item = document.createElement('li')
-    item.innerHTML = `<span>${data.name}</span>: ${data.message}`
-    messages.appendChild(item)
-    window.scrollTo(0, document.body.scrollHeight)
-})
+const keyPress = (e) => {
+	switch (e.keyCode) {
+		case 37:
+			if (userName === MainplayerName) {
+				if (count1 >= 0) {
+					count1 -= STEP;
+				}
+			} else {
+				if (count2 <= WIDTH - PLAYER_SIZE.x) {
+					count2 += STEP;
+				}
+			}
+			break;
+		case 39:
+			if (userName === MainplayerName) {
+				if (count1 <= WIDTH - PLAYER_SIZE.x) {
+					count1 += STEP;
+				}
+			} else {
+				if (count2 >= 0) {
+					count2 -= STEP;
+				}
+			}
+			break;
+	}
+	if (window.DeviceMotionEvent) {
+		window.addEventListener('devicemotion', motion_hook, false);
+	}
+	else {
+		// DeviceMotionEvent не поддерживается
+	}
+	 
+	// Обработчик события DeviceMotionEvent
+	function motion_hook(event) {
+		console.log(event);
+		divPlayer1.innerHTML = `Accelerometer:
+		X= ${event.accelerationIncludingGravity.x}
+		Y= ${event.accelerationIncludingGravity.y}
+		Z= ${event.accelerationIncludingGravity.z}`
+		// if (event.accelerationIncludingGravity.x > 90){count1++}
+		// if (event.accelerationIncludingGravity.x < 90){count1--}
+		// if (event.accelerationIncludingGravity.y > 90){count2++}
+		// if (event.accelerationIncludingGravity.y > 90){count2--}	
+		
+	}
+	if (userName === MainplayerName) {
+		socket.emit("pong", { value: count1, name: userName });
+	} else {
+		socket.emit("pong", { value: count2, name: userName });
+	}
+};
+addEventListener("keydown", keyPress);
+
+let drawP = (master, count, y) => {
+	if (master) {
+		ctx.fillStyle = P1_COLOR;
+		ctx.fillRect(
+			count,
+			HEIGHT - PLAYER_SIZE.y + y,
+			PLAYER_SIZE.x,
+			PLAYER_SIZE.y
+		);
+	} else {
+		ctx.fillStyle = P2_COLOR;
+		ctx.fillRect(count, 0 + y, PLAYER_SIZE.x, PLAYER_SIZE.y);
+	}
+};
+let drawBall = () => {
+	ctx.fillStyle = ball.color;
+	if (userName === MainplayerName) {
+		ctx.fillRect(count_ball.x, count_ball.y, ball.size.x, ball.size.y);
+	} else {
+		ctx.fillRect(
+			WIDTH - count_ball.x - ball.size.x,
+			HEIGHT - count_ball.y - ball.size.y,
+			ball.size.x,
+			ball.size.y
+		);
+	}
+};
+
+let mainLoop = () => {
+	ctx.fillStyle = BACKGROUND_COLOR;
+	ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+	if (userName === MainplayerName) {
+		drawP(true, count1, 0);
+		drawP(false, count2, 0);
+		// divPlayer1.innerHTML = `name: ${userName1}`;
+		divPlayer2.innerHTML = `name: ${userName2}`;
+	} else {
+		drawP(true, WIDTH - count1 - PLAYER_SIZE.x, -(HEIGHT - PLAYER_SIZE.y));
+		drawP(false, WIDTH - count2 - PLAYER_SIZE.x, HEIGHT - PLAYER_SIZE.y);
+		// divPlayer1.innerHTML = `name: ${userName2}`;
+		divPlayer2.innerHTML = `name: ${userName1}`;
+	}
+
+	drawBall();
+	socket.emit("ball", {});
+
+	requestAnimationFrame(mainLoop);
+};
+
+mainLoop();
--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-
-
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const PORT = 3001;
+let WIDTH = 600;
+let HEIGHT = 800;
+let player1_count = 0;
+let player2_count = 0;
+let MainplayerName = "";
+let ball = {
+	x: WIDTH / 2,
+	y: HEIGHT / 2,
+	size: 20,
+	color: "gray",
+	speed: 0.8,
+	speedX: 0,
+	speedY: 0,
+	angle: 45 * (Math.PI / 180),
+};
+ball.speedX = Math.cos(ball.angle) * ball.speed;
+ball.speedY = Math.sin(ball.angle) * ball.speed;
+
+const PLAYER_SIZE = { x: 100, y: 20 };
+let updateBall = () => {
+	ball.x += ball.speedX;
+	ball.y += ball.speedY;
+	if (
+		ball.x > player1_count &&
+		ball.x < player1_count + PLAYER_SIZE.x &&
+		ball.y + ball.size > HEIGHT - PLAYER_SIZE.y
+	) {
+		ball.angle = (Math.random() * (135 - 45) + 45) * (Math.PI / 180);
+		ball.speedX = Math.cos(ball.angle) * ball.speed;
+		ball.speedY = Math.sin(ball.angle) * ball.speed;
+		ball.speedY = -ball.speedY;
+	}
+	if (
+		ball.x > player2_count &&
+		ball.x < player2_count + PLAYER_SIZE.x &&
+		ball.y < PLAYER_SIZE.y
+	) {
+		ball.angle = (Math.random() * (315 - 225) + 225) * (Math.PI / 180);
+		ball.speedX = Math.cos(ball.angle) * ball.speed;
+		ball.speedY = Math.sin(ball.angle) * ball.speed;
+		ball.speedY = -ball.speedY;
+	}
+
+	if (ball.y >= HEIGHT) {
+		ball.y = HEIGHT / 2;
+		ball.angle = (Math.random() * (135 - 45) + 45) * (Math.PI / 180);
+		ball.speedX = Math.cos(ball.angle) * ball.speed;
+		ball.speedY = Math.sin(ball.angle) * ball.speed;
+	}
+	if (ball.x >= WIDTH - ball.size || ball.x <= 0) {
+		ball.speedX *= -1;
+	}
+	if (ball.y < 0) {
+		ball.y = HEIGHT / 2;
+		ball.angle = (Math.random() * (315 - 225) + 225) * (Math.PI / 180);
+		ball.speedX = Math.cos(ball.angle) * ball.speed;
+		ball.speedY = Math.sin(ball.angle) * ball.speed;
+	}
+};
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
@@ -13,10 +70,34 @@ app.get("/", (req, res) => {
 app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
-	socket.on("chat message", (data) => {
-		io.emit("chat message", {
-			message: data.message,
-			name: data.name,
+	socket.on("pong", (data) => {
+
+		if (MainplayerName === "") {
+			MainplayerName = data.name;
+		}
+		if (data.name === MainplayerName) {
+			player1_count = data.value;
+
+			io.emit("pong", {
+				value: player1_count,
+				name: data.name,
+				mainName: MainplayerName,
+			});
+		} else {
+			player2_count = data.value;
+
+			io.emit("pong", {
+				value: player2_count,
+				name: data.name,
+				mainName: MainplayerName,
+			});
+		}
+	});
+	socket.on("ball", (data) => {
+		updateBall();
+
+		io.emit("ball", {
+			ball: { x: ball.x, y: ball.y },
 		});
 	});
 });
